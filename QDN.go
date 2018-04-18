f746b479d0c721bf3b908f0247ebd190872aa067
4kills <dominik.ochs@gmx.net>
2018-04-18 20:26:14 +0200

    fixed stupid pointer bug

--- QDN.go
+++ QDN.go
@@ -20,7 +20,7 @@ func (r *Raw) Format() error {
 
 // Unmarshal fills a given interface with the corresponding qdn byte data.
 func Unmarshal(stru interface{}, data []byte) error {
-	if reflect.TypeOf(stru).Kind() != reflect.Struct {
+	if reflect.Indirect(reflect.ValueOf(stru)).Kind() != reflect.Struct {
 		return errors.New("qdn.Unmarshal error: Provided parameter is no struct pointer")
 	}
 
